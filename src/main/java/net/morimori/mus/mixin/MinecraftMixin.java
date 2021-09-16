15beb834eb69eaf33166bd97e5f8b1981028b243
Klotzi111 <>
2021-09-16 17:09:21 +0200

    Added bar for current allocation size

--- src/main/java/net/morimori/mus/mixin/MinecraftMixin.java
+++ src/main/java/net/morimori/mus/mixin/MinecraftMixin.java
@@ -1,21 +1,15 @@
 package net.morimori.mus.mixin;
 
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.morimori.mus.MemoryUsageScreen;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-	@Shadow
-	@Nullable
-	public LocalPlayer player;
 
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void tick(CallbackInfo ci) {
