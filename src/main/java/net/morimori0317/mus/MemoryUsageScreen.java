ec91726107f1c47d8c352714f1e0cea60682134c
MORIMORI0317 <morimori.0317@outlook.jp>
2022-01-09 09:34:34 +0900

    fix server list FML incompatibility notification

--- src/main/java/net/morimori0317/mus/MemoryUsageScreen.java
+++ src/main/java/net/morimori0317/mus/MemoryUsageScreen.java
@@ -3,9 +3,12 @@ package net.morimori0317.mus;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 import org.lwjgl.glfw.GLFW;
 
 @Mod(MemoryUsageScreen.MODID)
@@ -13,8 +16,8 @@ public class MemoryUsageScreen {
     public static final String MODID = "memoryusagescreen";
     public static final KeyMapping showMemoryBarKey = new KeyMapping("key.memoryusagescreen.show", GLFW.GLFW_KEY_U, "key.categories.memoryusagescreen");
 
-
     public MemoryUsageScreen() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         ClientConfig.init();
     }
